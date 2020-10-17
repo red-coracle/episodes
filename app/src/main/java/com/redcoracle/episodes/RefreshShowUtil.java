f09f93d8e44b0db05237880774e3691f90e8bced
André Birke <abirke@posteo.de>
2020-10-17 10:14:39 +0200

    Address review comment to remove unused line

--- app/src/main/java/com/redcoracle/episodes/RefreshShowUtil.java
+++ app/src/main/java/com/redcoracle/episodes/RefreshShowUtil.java
@@ -43,11 +43,10 @@ public class RefreshShowUtil {
 		Log.i(TAG, String.format("Refreshing show %d", showId));
 
 		SharedPreferences preferences = Preferences.getSharedPreferences();
-		preferences.getString("pref_language", "en");
-
-		final int showTvdbId = getShowTvdbId(showId, contentResolver);
 		String showLanguage = preferences.getString("pref_language", "en");
+
 		// fetch full show + episode information from tvdb
+		final int showTvdbId = getShowTvdbId(showId, contentResolver);
 		final Show show = tvdbClient.getShow(showTvdbId, showLanguage);
 
 		if (show != null) {
